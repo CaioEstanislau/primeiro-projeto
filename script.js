32091bde5ab89b1596276269e4e014b838b6051a
CaioEstanislau <contatocaiosamuel.e@gmail.com>
2024-05-22 23:49:09 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function toggleMode() {
+    const body = document.body
+    body.classList.toggle("light")
+  
+  // (mostrando oque o codigo acima faz)
+  // if(body.classList.contains('light')) {
+  //     body.classList.remove('light')   
+  // } else {
+  //     body.classList.add('light')
+  // } 
+
+    const img = document.querySelector("#profile img")
+    
+    if (body.classList.contains('light')){
+        img.setAttribute('src', './assets/avatar-light.png')
+        img.setAttribute('alt', 'imageprofileLight')
+    } else {
+        img.setAttribute ('src', './assets/avatar.png')
+        img.setAttribute ('alt', 'imageprofileDark')    }
+    
+  
+
+  
+    
+
+}
